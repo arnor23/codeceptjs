4252d15b7c26e7e066d25cf3d45d8356fc1491ef
arnor <arnorguzman23@gmail.com>
2025-04-06 21:18:10 -0400

    fix main page locator

--- pages/main_page.js
+++ pages/main_page.js
@@ -3,7 +3,7 @@ const { I } = inject();
 class MainPage {
     constructor() {
         this.locator = {
-            signIn: 'img[src="https://todo.ly/Images/design/pagelogin.png"]',
+            signIn: 'a > img[src="https://todo.ly/Images/design/pagelogin.png"]',
             email: 'ctl00_MainContent_LoginControl1_TextBoxEmail',
             password: '#ctl00_MainContent_LoginControl1_TextBoxPassword',
             loginButton: '#ctl00_MainContent_LoginControl1_ButtonLogin'
